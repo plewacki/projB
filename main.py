3a775db75120684edc1f7a6f84964923cbe36239
Plewacki z Protonmail <plewacki@protonmail.com>
2022-05-10 10:53:54 +0200

    ok

--- main.py
+++ main.py
@@ -5,4 +5,12 @@ a = int(input("Podaj ilość:"))
 for x in range(a):
     print(x)
 
-print("Zmienna licznik po zakończeniu = ",x)
+print("Zmienna licznik po zakończeniu = ", x)
+
+
+# tu będzie funkcja
+def doKwadratru(x):
+    return x ** 2
+
+
+print(x, "do kwadratu = ", doKwadratru(x))
